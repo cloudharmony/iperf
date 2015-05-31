c282527ef77b6279e3a83efbbdae551cfeb52986
Jason Read <jason@cloudharmony.com>
2015-05-30 21:29:46 -0700

    correctly identify iperf version in cygwin environment

--- lib/IperfTest.php
+++ lib/IperfTest.php
@@ -923,7 +923,7 @@ class IperfTest {
     $this->getRunOptions();
     $rrdStarted = isset($this->options['collectd_rrd']) ? ch_collectd_rrd_start($this->options['collectd_rrd_dir'], $this->verbose) : FALSE;
     $iperf3 = FALSE;
-    if (preg_match('/\s([0-9\.]+)[\s\-C]/', trim(shell_exec(sprintf('%s --version 2>&1', $this->iperf))), $m)) {
+    if (preg_match('/\s([0-9\.]+)/m', trim(shell_exec(sprintf('%s --version 2>&1', $this->iperf))), $m)) {
       $this->options['iperf_version'] = $m[1];
       $iperf3 = substr($m[1], 0, 1) == 3;
     }
