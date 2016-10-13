e908dae09ed80b0894c31d334007dbd9a490c8e4
Jason Read <jason@cloudharmony.com>
2016-10-13 08:14:52 -0700

    wkhtmltoimage bug fix

--- lib/IperfTest.php
+++ lib/IperfTest.php
@@ -459,7 +459,7 @@ class IperfTest {
         if (IperfTest::wkhtmltopdfInstalled()) {
           $cmd = sprintf('%swkhtmltoimage %s %s >/dev/null 2>&1', isset($this->options['wkhtml_xvfb']) ? 'xvfb-run ' : '', $img, $png = str_replace('.svg', '.png', $img));
           $ecode = trim(exec($cmd));
-          if ($ecode > 0 || !file_exists($png) || !filesize($png)) print_msg(sprintf('Unable to convert SVG image %s to PNG %s (exit code %d)', $img, $png, $ecode), $this->verbose, __FILE__, __LINE__, TRUE);
+          if (!file_exists($png) || !filesize($png)) print_msg(sprintf('Unable to convert SVG image %s to PNG %s (exit code %d)', $img, $png, $ecode), $this->verbose, __FILE__, __LINE__, TRUE);
           else {
             exec(sprintf('rm -f %s', $img));
             print_msg(sprintf('SVG image %s converted to PNG successfully - PNG will be used in report', basename($img)), $this->verbose, __FILE__, __LINE__);
